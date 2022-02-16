da246640346e37c293fd33e011c5a82c87de87c1
Diego <poncelas91@gmail.com>
2022-02-16 15:33:21 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+PROJECT - TELEGRAM BOT
+
+
+
+
+NOTES:
+-https://python-telegram-bot.readthedocs.io/en/latest/telegram.ext.callbackcontext.html
+-https://python-telegram-bot.readthedocs.io/en/latest/telegram.update.html
+-Telegram API only supports UTF-8, but Python works with Unicode
+-Emojis in Unicode begins with U0000 or U0001
+"""
+
+
